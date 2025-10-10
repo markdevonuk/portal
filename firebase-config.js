53701e1ba3951a535f323ee9629837099b680245
markdevonuk <markdevon@gmail.com>
2025-10-10 16:03:04 +0100

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// firebase-config.js
+export const firebaseConfig = {
+  apiKey: "AIzaSyDTkfw7tjrPkGLGHCnYgUhX8lsY9w7zn5c",
+  authDomain: "fmsportal-dbfb3.firebaseapp.com",
+  projectId: "fmsportal-dbfb3",
+  storageBucket: "fmsportal-dbfb3.firebasestorage.app",
+  messagingSenderId: "411503752161",
+  appId: "1:411503752161:web:4df9f2d2568a2d6e88d8a0",
+  measurementId: "G-K6WKLHP087"
+};
